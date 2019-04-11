6284390046c453f1f4df4717a2a9b3678e069dcf
CodingMonster01 <747616936@qq.com>
2019-04-11 15:05:53 +0800

    Rftghjkl;

--- hanwinCamera/www/hanwinCamera.js
+++
@@ -1,8 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'hanwinCamera', 'coolMethod', [arg0]);
-};
-exports.getCamera = function (imgUrl, success, error) {
-    exec(success, error, 'hanwinCamera', 'getCamera', [imgUrl]);
-}
